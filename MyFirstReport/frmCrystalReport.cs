7bbc7e04f7627edd376b38c6117f3469fbb6243d
1460691 <ly.camnhi96@gmail.com>
2017-06-17 22:54:19 +0700

    MyFirstReport window form ProductsReport WPF chưa xong, còn add reference

---
+++ MyFirstReport/frmCrystalReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstReport
+{
+    public partial class frmCrystalReport : Form
+    {
+        public frmCrystalReport()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCrystalReport_Load(object sender, EventArgs e)
+        {
+            ProductsList rpt = new ProductsList();
+            crystalReportViewer1.ReportSource = rpt;
+        }
+    }
+}
