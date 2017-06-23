1c095eb9d2a3c53a70d59a2d7658f9155d7261ed
TUANNGUYEN\Tuan Nguyen <upinguyen96@gmail.com>
2017-06-24 00:18:50 +0700

    up ảnh listbox, in hóa đơn ra excel

--- View/QLNhapHang.xaml.cs
+++ View/QLNhapHang.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using ViewModel;
+using Excel = Microsoft.Office.Interop.Excel;
+//using Microsoft.Office.Interop.Excel;
 
 namespace View
 {
@@ -250,6 +254,31 @@ namespace View
             txtGia.Text = "";
             txtSoLuong.Text = "";
             txtTongTien.Text = "";
+        }
+
+        private void btnInHD_Click(object sender, RoutedEventArgs e)
+        {
+            Excel.Application excel = new Excel.Application();
+            excel.Visible = true;
+            Excel.Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+            Excel.Worksheet sheet1 = (Excel.Worksheet)workbook.Sheets[1];
+
+            for (int j = 0; j < dataGrid.Columns.Count; j++) 
+            {
+                Excel.Range myRange = (Excel.Range)sheet1.Cells[1, j + 1];
+                sheet1.Cells[1, j + 1].Font.Bold = true; 
+                sheet1.Columns[j + 1].ColumnWidth = 15;
+                myRange.Value2 = dataGrid.Columns[j].Header;
+            }
+            for (int i = 0; i < dataGrid.Columns.Count; i++)
+            { 
+                for (int j = 0; j < dataGrid.Items.Count; j++)
+                {
+                    TextBlock b = dataGrid.Columns[i].GetCellContent(dataGrid.Items[j]) as TextBlock;
+                    Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[j + 2, i + 1];
+                    myRange.Value2 = b.Text;
+                }
+            }
         }
     }
 }
