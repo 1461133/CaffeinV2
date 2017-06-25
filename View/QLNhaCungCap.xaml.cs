b3e7f93b1290e6ae626c6703108cf10b2dc9a150
TUANNGUYEN\Tuan Nguyen <upinguyen96@gmail.com>
2017-06-25 12:13:20 +0700

    ql nha cung cap

--- View/QLNhaCungCap.xaml.cs
+++ View/QLNhaCungCap.xaml.cs
@@ -19,46 +19,11 @@ namespace CAFFEIN
     /// </summary>
     public partial class QLNhaCungCap : Window
     {
-       //private CAFEINDataContext db = new CAFEINDataContext();
         public QLNhaCungCap()
         {
             InitializeComponent();
-            Refresh();
         }
 
-        private void Refresh()
-        {
-            //this.DataContext = db.tb_NCC.ToList();
-        }
-
-        private void BtnThemNCC_OnMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            /*if ((db.tb_NCC.SingleOrDefault(c => c.mancc == txtid.Text)) != null)
-            {
-                MessageBox.Show("Ma NCC bi trung!!!");
-                return;
-            }
-            db.tb_NCC.Add(new tb_NCC() { mancc = txtid.Text, diachi = txtdc.Text, sdt = txtsdt.Text, tenncc = txtname.Text});
-            db.SaveChanges();
-            Refresh();*/
-        }
-
-        private void BtnSuaNCC_OnMouseDown(object sender, MouseButtonEventArgs e)
-        {
-           /* var ncc = (tb_NCC) DataGrid.SelectedItem;
-            ncc.mancc = txtid.Text;
-            ncc.diachi = txtdc.Text;
-            ncc.sdt = txtsdt.Text;
-            ncc.tenncc = txtname.Text;
-            db.SaveChanges();
-            Refresh();*/
-        }
-
-        private void BtnXoaNCC_OnMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            /*db.tb_NCC.Remove((tb_NCC) DataGrid.SelectedItem);
-            db.SaveChanges();
-            Refresh();*/
-        }
+        
     }
 }
--- ViewModel/NhaCC.cs
+++ ViewModel/NhaCC.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,46 @@ namespace ViewModel
 {
     public class NhaCC
     {
+        public bool KTNhaCC(string ID)
+        {
+            using (var qlcf = new QL_QuancapheEntities())
+            {
+                int n = qlcf.tb_NCC.Where(m => m.mancc == ID).Count();
+                if (n > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int ThemNhaCC(string manhacc, string tennhacc, string dc,string dt )
+        {
+            int kq = 0;
+            using (var qlcf = new QL_QuancapheEntities())
+            {
+                if (KTNhaCC(manhacc) == false)
+                {
+
+                    var ncc = new tb_NCC { mancc = manhacc, tenncc = tennhacc, diachi = dc, sdt = dt, trangthai = true };
+                    qlcf.tb_NCC.Add(ncc);
+                    if (qlcf.SaveChanges() > 0)
+                    {
+                        kq = 1;
+                    }
+                    else
+                    {
+                        kq = 0;
+                    }
+                }
+                else
+                {
+                    kq = 0;
+                }
+
+            }
+            return kq;
+        }
+
     }
 }
